37d0af756ee8ac683ef246122bfca41cd5c4fb84
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 23:13:10 +0100

    Creation

---
+++ src/thi05_J_CSI/Container.java
@@ -0,0 +1,29 @@
+package thi05_J_CSI;
+
+import java.util.Random;
+import java.util.Vector;
+
+public final class Container implements Runnable {
+	
+	private final Vector<Integer> vector = new Vector<Integer>(1000);
+	public Vector<Integer> getVector() { 
+		return vector;
+	}
+	
+	@Override public synchronized void run() { 
+		Random number = new Random(123L);
+		int i = vector.capacity();
+		while (!Thread.interrupted() && i > 0) {
+			vector.add(number.nextInt(100));
+			i--; 
+			}
+	}
+	
+	public static void main(String[] args) throws InterruptedException { 
+		Container c = new Container();
+		Thread thread = new Thread(c);
+		thread.start();
+	    Thread.sleep(5000);
+	    thread.interrupt();
+	  }
+	}
